aa66381befc1b1537d3c5ab74ce5ba6b8c61a391
kodai maeda <isleep4070@icloud.com>
2024-03-05 15:09:02 +0900

    add:定期的にlineメッセージを送れるように実装

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, 'log/cron.log'
 
 
-every 1.day, at: '9:00 am' do
-  runner "ContactLensNotificationJob.perform_now"
-end
+
+env :PATH, ENV['PATH']
+job_type :runner, "cd :path && rbenv exec bundle exec rails runner -e :environment ':task' :output"
+
+# every 1.minute do
+#   runner "Lens.send_test_notification"
+# end
+
+
+
+# every 1.day, at: '9:00 am' do
+#   runner "Lens.check_and_notify"
+# end
 
 
